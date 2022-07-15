f3e003c1570001fea7c77ad410d0d0e880ff137c
LunaRocio <r.lunamerchan@gmail.com>
2022-07-15 18:46:02 +0200

    add a try of fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hola');
+
+const listSerie = document.querySelector('.js-serie');
+let animeSeriesList =[];
+
+
+fetch("https://api.jikan.moe/v4/anime?q=")
+  .then((response=> response.json()))
+  .then((data) =>{
+    animeSeriesList=data.data;
+    console.log(animeSeriesList);
+    console.log(animeSeriesList[1].images.jpg.image_url);
+    listSerie.innerHTML= `<li><img
+      src=${animeSeriesList[1].images.jpg.image_url}
+      alt=${animeSeriesList[1].title}
+    />
+    <h2>${animeSeriesList[1].title}</h2>
+  </li>`;
+    console.log(listSerie);
+  });
+
+
